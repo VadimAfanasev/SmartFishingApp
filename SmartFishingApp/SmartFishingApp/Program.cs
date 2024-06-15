43e570f684e1b024ceb4ca96a45174264949220a
Vadim Afanasev <trim-agency@yandex.ru>
2024-06-15 20:52:52 +0700

    Много правок. В основном исправлял ошибки в совместимостях библиотек

--- SmartFishingApp/SmartFishingApp/Program.cs
+++ SmartFishingApp/SmartFishingApp/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddSwaggerGen();
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connection));
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
