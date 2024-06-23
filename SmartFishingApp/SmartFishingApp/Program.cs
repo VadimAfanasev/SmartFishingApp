005273526f87e8d0ffb17951d5c8cf78df45dcec
Vadim Afanasev <trim-agency@yandex.ru>
2024-06-23 19:54:48 +0700

    Прокинул DI, навел порядок в Startup, протестировал работу

--- SmartFishingApp/SmartFishingApp/Program.cs
+++ SmartFishingApp/SmartFishingApp/Program.cs
@@ -1,5 +1,9 @@
 using Database.Context;
+using Interfaces.UserProfile.FishingReel;
 using Microsoft.EntityFrameworkCore;
+using Services.UserProfile.FishingReel;
+using SmartFishingApp.Services.Implementations.FishingReelServices;
+using SmartFishingApp.Services.Interfaces;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +18,8 @@ builder.Services.AddSwaggerGen();
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connection));
 
+RegisterMappingServices(builder.Services);
+RegisterSmartFishingAppServices(builder.Services);
 
 var app = builder.Build();
 
@@ -31,3 +37,13 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void RegisterMappingServices(IServiceCollection services)
+{
+    services.AddScoped<IFishingReelMapperService, FishingReelMapperService>();
+}
+
+static void RegisterSmartFishingAppServices(IServiceCollection services)
+{
+    services.AddScoped<IFishingReelService, FishingReelService>();
+}
