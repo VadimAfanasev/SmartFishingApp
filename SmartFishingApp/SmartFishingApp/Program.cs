119edd5681d981cc655f9658437f9cc32415e933
Vadim Afanasev <trim-agency@yandex.ru>
2024-07-03 21:53:08 +0700

    Закончил с эндпоинтами для Rod.

--- SmartFishingApp/SmartFishingApp/Program.cs
+++ SmartFishingApp/SmartFishingApp/Program.cs
@@ -1,9 +1,13 @@
 using Database.Context;
 using Interfaces.UserProfile.FishingReel;
+using Interfaces.UserProfile.Rod;
 using Microsoft.EntityFrameworkCore;
 using Services.UserProfile.FishingReel;
+using Services.UserProfile.Rod;
 using SmartFishingApp.Services.Implementations.FishingReelServices;
+using SmartFishingApp.Services.Implementations.RodServices;
 using SmartFishingApp.Services.Interfaces;
+using SmartFishingApp.Services.Interfaces.IRodServices;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -42,10 +46,12 @@ static void RegisterMappingServices(IServiceCollection services)
 {
     services.AddScoped<IFishingReelMapperService, FishingReelMapperService>();
     services.AddScoped<IFishingReelTypeMapperService, FishingReelTypeMapperService>();
+    services.AddScoped<IRodMapperService, RodMapperService>();
 }
 
 static void RegisterSmartFishingAppServices(IServiceCollection services)
 {
     services.AddScoped<IFishingReelService, FishingReelService>();
     services.AddScoped<IFishingReelTypeService, FishingReelTypeService>();
+    services.AddScoped<IRodService, RodService>();
 }
