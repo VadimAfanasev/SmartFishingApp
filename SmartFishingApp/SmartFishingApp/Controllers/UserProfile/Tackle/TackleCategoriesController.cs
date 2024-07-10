a6ae9a8085bb040e2b0e783d1bfaa0ae78e4bca5
Vadim Afanasev <trim-agency@yandex.ru>
2024-07-10 23:03:00 +0700

    Приступил к работе над эндпоинтами типа рыболовных приманок

--- SmartFishingApp/SmartFishingApp/Controllers/UserProfile/Tackle/TackleCategoriesController.cs
+++ SmartFishingApp/SmartFishingApp/Controllers/UserProfile/Tackle/TackleCategoriesController.cs
@@ -8,5 +8,13 @@ namespace SmartFishingApp.Controllers.UserProfile.Tackle;
 [Route("api/tackle-categories")]
 public class TackleCategoriesController : ControllerBase
 {
+    /// <summary>
+    ///     Сервис Маппинга сущностей типов рыболовных приманок.
+    /// </summary>
+    private readonly ITackleCategoryMapperService _tackleCategoryMapperService;
     
+    /// <summary>
+    ///     Сервис для работы с типами рыболовных приманок.
+    /// </summary>
+    private readonly ITackleCategoryService _tackleCategoryService;
 }
